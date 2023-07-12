5895a9ba633ca24e4548efbca28381df6b5f8572
Aney <aneyjogi23@gmail.com>
2023-07-12 11:34:15 +0530

    kala first commit

---
+++ src/test/java/NewPackage/kalaTest.java
@@ -0,0 +1,15 @@
+package NewPackage;
+
+import org.testng.annotations.Test;
+
+public class kalaTest {
+	
+	@Test
+	public void kalaMethodTest()
+	{
+		System.out.println("Kala1");
+	}
+	
+	
+
+}
--- src/test/java/com/OnlineEmart/ParameterTest.java
+++ src/test/java/com/OnlineEmart/ParameterTest.java
@@ -3,15 +3,16 @@ package com.OnlineEmart;
 import org.testng.annotations.Test;
 
 public class ParameterTest {
-	
+
 	@Test
 	public void ParametersConfiguration()
 	{
-		String b = System.getProperty("browser");
-		System.getProperty("username");
-		System.getProperty("password");
-		System.out.println(b);
+		String Browser = System.getProperty("browser");
+		String Username=System.getProperty("username");
+		String Password= System.getProperty("password");
+		System.out.println(Browser);
+		System.out.println(Username);
+		System.out.println(Password);
     }
-
-
+	
 }
