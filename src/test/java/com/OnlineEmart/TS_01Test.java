43c30af23ba3e6cfba5aafe9ff40d13fe8b33da2
Aney <aneyjogi23@gmail.com>
2023-07-14 13:19:08 +0530

    14/07 commit

--- src/test/java/com/OnlineEmart/TS_01Test.java
+++ src/test/java/com/OnlineEmart/TS_01Test.java
@@ -1,5 +1,6 @@
 package com.OnlineEmart;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TS_01Test {
@@ -13,7 +14,9 @@ public class TS_01Test {
 	@Test
 	public void sampleTest2()
 	{
+		Assert.fail();
 		System.out.println("test script 02");
+		
 	}
 
 }
